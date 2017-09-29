0414c5fdeba01a95367adaa1d83ffc58280b0636
yangyu <591282626@qq.com>
2017-09-29 10:20:06 +0800

    测试1-2

--- js/mask.js
+++ js/mask.js
@@ -5,7 +5,6 @@ $('.fr>a').on('click',function () {
     showOverlay();
     $('.hj').css('display','block');
     $('.hj').css('position' ,'absolute').css('left',left + scrollLeft);
-    $('html,body').animate({scrollTop: '0px'}, 100); 
     $('.hj').bind("touchmove",function(e){
         e.preventDefault();
     });
